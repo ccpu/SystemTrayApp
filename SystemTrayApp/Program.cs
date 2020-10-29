fb095d4eb3cf915681ea29dd0332be58b808d465
cyrus <mdoaie@yahoo.co.uk>
2020-10-29 15:00:15 +0330

    Add project files.

---
+++ SystemTrayApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SystemTrayApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (System.Diagnostics.Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (new SystemTrayMenu())
+            {
+                Application.Run();
+            }
+
+        }
+    }
+}
---
+++ SystemTrayApp/SystemTrayMenu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SystemTrayApp
+{
+    public partial class SystemTrayMenu : Form
+    {
+        public SystemTrayMenu()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+
+            // To provide your own custom icon image, go to:
+            //   1. Project > Properties... > Resources
+            //   2. Change the resource filter to icons
+            //   3. Remove the Default resource and add your own
+            //   4. Modify the next line to Properties.Resources.<YourResource>
+            this.Icon = Properties.Resources.Default;
+            this.SystemTrayIcon.Icon = Properties.Resources.Default;
+
+            // Change the Text property to the name of your application
+            this.SystemTrayIcon.Text = "System Tray App";
+            this.SystemTrayIcon.Visible = true;
+
+            // Modify the right-click menu of your system tray icon here
+            ContextMenu menu = new ContextMenu();
+            menu.MenuItems.Add("Exit", ContextMenuExit);
+            this.SystemTrayIcon.ContextMenu = menu;
+
+            this.Resize += WindowResize;
+            this.FormClosing += WindowClosing;
+        }
+        
+        private void SystemTrayIconDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void ContextMenuExit(object sender, EventArgs e)
+        {
+            this.SystemTrayIcon.Visible = false;
+            Application.Exit();
+            Environment.Exit(0);
+        }
+
+        private void WindowResize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void WindowClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
